cec270ad9fefc334739d914db59b887b1ea5dcb0
Vallabh Ashish Kedarisetti <ashish.kedarisetti@protegrity.com>
2021-01-04 10:41:28 +0530

    Day 4

---
+++ 1672/1672.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        m=0
+        for i in accounts:
+            if(sum(i)>m):
+                m=sum(i)
+        return m
