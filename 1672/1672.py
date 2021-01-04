67ee963bd2d7df11be567c9838c3e8ff61c0b077
Coderash1998 <47770728+Coderash1998@users.noreply.github.com>
2021-01-04 10:57:17 +0530

    Add files via upload

---
+++ 1672/1672.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        m=0
+        for i in accounts:
+            if(sum(i)>m):
+                m=sum(i)
+        return m
+            
