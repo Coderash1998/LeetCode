fcbf362ad5395e58349009bcf70476f995ee893c
Coderash1998 <47770728+Coderash1998@users.noreply.github.com>
2021-05-01 13:20:47 +0530

    Add files via upload

---
+++ 1779/1779.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
+        m=10000
+        for i in points:
+            if i[0]==x or i[1]==y:
+                if m>abs(x-i[0])+abs(y-i[1]):
+                    m=min(m,abs(x-i[0])+abs(y-i[1]))
+                    o=points.index(i)
+        if m==10000:
+            return -1
+        return o
